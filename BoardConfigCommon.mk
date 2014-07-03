5305ef7b11f56ead997c7d5f4834baa86035a703
utopiar <zhengdaxg@163.com>
2014-07-03 17:44:14 +0800

    first commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,99 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+BOARD_VENDOR := xiaomi-qcom
+
+# Platform
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+TARGET_BOARD_PLATFORM := msm8226
+TARGET_BOOTLOADER_BOARD_NAME := MSM8226
+TARGET_CPU_VARIANT := krait
+TARGET_USE_KINGFISHER_OPTIMIZATION := true
+
+-include device/xiaomi/qcom-common/BoardConfigCommon.mk
+
+LOCAL_PATH := device/xiaomi/msm8226-common
+
+# this will lead to compile v8 module faile
+#TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
+
+# Inline kernel building
+#BOARD_KERNEL_SEPARATED_DT := true
+#BOARD_CUSTOM_BOOTIMG_MK := device/motorola/msm8226-common/mkbootimg.mk
+#TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
+#TARGET_KERNEL_CONFIG := msm8226_mmi_defconfig
+#BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.write_protect=0 zcache
+#BOARD_KERNEL_BASE := 0x00000000
+#BOARD_KERNEL_PAGESIZE := 2048
+#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+
+#WLAN_MODULES:
+#	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+#	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+#	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
+
+#TARGET_KERNEL_MODULES += WLAN_MODULES
+
+# QCOM BSP
+TARGET_USES_QCOM_BSP := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+
+# Audio
+AUDIO_FEATURE_DISABLED_FM :=
+AUDIO_FEATURE_DISABLED_SSR := true
+BOARD_HAVE_QCOM_FM := true
+AUDIO_FEATURE_DISABLED_ANC_HEADSET := true
+AUDIO_FEATURE_DISABLED_DS1_DOLBY_DDP := true
+
+# GPS
+TARGET_NO_RPC := true
+
+# Graphics
+BOARD_EGL_CFG := $(LOCAL_PATH)/config/egl.cfg
+TARGET_DISPLAY_USE_RETIRE_FENCE :=
+TARGET_QCOM_DISPLAY_VARIANT := caf-new
+
+# Enables Adreno RS driver
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+TARGET_QCOM_MEDIA_VARIANT := caf-new
+
+# Use qcom power hal
+TARGET_POWERHAL_VARIANT := qcom
+
+# Enable CPU boosting events in the power HAL
+TARGET_USES_CPU_BOOST_HINT := true
+
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Hardware tunables framework
+BOARD_HARDWARE_CLASS := device/xiaomi/msm8226-common/cmhw/
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := armani
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_RECOVERY_SWIPE := true
